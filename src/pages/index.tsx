6bb24bc558d579755b4b0ab3cbfa503c54a378ed
haiche1410 <70754587+haiche1410@users.noreply.github.com>
2022-01-19 22:46:30 +0700

    Enter

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,9 +3,7 @@ import styles from "./index.less";
 export default function IndexPage() {
   return (
     <div>
-      <h1 className={styles.title}>Welcome to My Page React sample CICD and Jenkins</h1>
-      <br>
-      <h1 className={styles.title}>Auto Jenkins</h1>
+      <h1 className={styles.title}>Welcome to My Page React sample CICD and Jenkins auto deploy</h1>
     </div>
   );
 }
