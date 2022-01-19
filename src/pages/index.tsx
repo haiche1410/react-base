a0361fc6e107fbcfc525dc472c1bcccce72be737
haiche1410 <70754587+haiche1410@users.noreply.github.com>
2022-01-19 22:43:44 +0700

    Enter

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,6 +4,8 @@ export default function IndexPage() {
   return (
     <div>
       <h1 className={styles.title}>Welcome to My Page React sample CICD and Jenkins</h1>
+      <br>
+      <h1 className={styles.title}>Auto Jenkins</h1>
     </div>
   );
 }
